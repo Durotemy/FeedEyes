eceea285df69671b13117abab9afd793d7251a9e
durotemy <durotemy@gmail.com>
2022-10-12 11:53:04 +0100

    folder and file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import useStyles from './styles';
+import { Actors, Movies, Profile, NavBar, MovieInformation } from '.';
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Router>
+        <CssBaseline />
+        <NavBar />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/actors" element={<Actors />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInformation />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
 };
 export default App;
---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Movies = () => {
+  // eslint-disable-next-line no-console
+  console.log('hha');
+  return (
+    <div>Movies</div>
+  );
+};
+
+export default Movies;
